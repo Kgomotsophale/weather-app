bb7685238054b09a0869dad8fae4aa1d20991941
Kgomotsophale <145598796+Kgomotsophale@users.noreply.github.com>
2025-02-26 16:13:13 +0200

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=2a2d3781d11f2f8ba38a2a3338227f1c&units=metric`
+      )
+      .then((response) => setWeather(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      {weather ? (
+        <div>
+          <p>City: {weather.name}</p>
+          <p>Temperature: {weather.main.temp}°C</p>
+          <p>Weather: {weather.weather[0].description}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
